10bab2738f1363c23da07f0d19fd207590fe6793
canhkhoa <thanhnock12346789@gmail.com>
2024-08-01 17:35:22 +0700

    First

---
+++ Duan1/Solution2/GUI/FDatBanAn.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace GUI
+{
+    partial class FDatBanAn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbTotalPrice = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lsvBill = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.flpFood = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpCategory = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpTable = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.txtTenBan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txbTotalPrice
+            // 
+            this.txbTotalPrice.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbTotalPrice.BackColor = System.Drawing.Color.White;
+            this.txbTotalPrice.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txbTotalPrice.ForeColor = System.Drawing.Color.Black;
+            this.txbTotalPrice.Location = new System.Drawing.Point(934, 132);
+            this.txbTotalPrice.Margin = new System.Windows.Forms.Padding(4);
+            this.txbTotalPrice.Name = "txbTotalPrice";
+            this.txbTotalPrice.ReadOnly = true;
+            this.txbTotalPrice.Size = new System.Drawing.Size(161, 43);
+            this.txbTotalPrice.TabIndex = 25;
+            this.txbTotalPrice.Text = "0";
+            this.txbTotalPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(934, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 68);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Quay lại";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(934, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 68);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Chọn bàn ăn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lsvBill
+            // 
+            this.lsvBill.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsvBill.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lsvBill.GridLines = true;
+            this.lsvBill.HideSelection = false;
+            this.lsvBill.Location = new System.Drawing.Point(871, 219);
+            this.lsvBill.Margin = new System.Windows.Forms.Padding(4);
+            this.lsvBill.Name = "lsvBill";
+            this.lsvBill.Size = new System.Drawing.Size(348, 441);
+            this.lsvBill.TabIndex = 20;
+            this.lsvBill.UseCompatibleStateImageBehavior = false;
+            this.lsvBill.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Tên món";
+            this.columnHeader1.Width = 133;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Số lượng";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Đơn giá";
+            this.columnHeader3.Width = 54;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Thành tiền";
+            this.columnHeader4.Width = 99;
+            // 
+            // flpFood
+            // 
+            this.flpFood.Location = new System.Drawing.Point(276, 233);
+            this.flpFood.Name = "flpFood";
+            this.flpFood.Size = new System.Drawing.Size(552, 348);
+            this.flpFood.TabIndex = 19;
+            // 
+            // flpCategory
+            // 
+            this.flpCategory.AutoScroll = true;
+            this.flpCategory.Location = new System.Drawing.Point(109, 233);
+            this.flpCategory.Name = "flpCategory";
+            this.flpCategory.Size = new System.Drawing.Size(161, 348);
+            this.flpCategory.TabIndex = 18;
+            // 
+            // flpTable
+            // 
+            this.flpTable.AutoScroll = true;
+            this.flpTable.Location = new System.Drawing.Point(109, 3);
+            this.flpTable.Name = "flpTable";
+            this.flpTable.Size = new System.Drawing.Size(719, 178);
+            this.flpTable.TabIndex = 17;
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiem.Location = new System.Drawing.Point(238, 185);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(245, 43);
+            this.txtTimKiem.TabIndex = 26;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.BackColor = System.Drawing.Color.White;
+            this.btnTimKiem.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.ForeColor = System.Drawing.Color.Black;
+            this.btnTimKiem.Location = new System.Drawing.Point(109, 186);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(123, 32);
+            this.btnTimKiem.TabIndex = 27;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = false;
+            // 
+            // txtTenBan
+            // 
+            this.txtTenBan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTenBan.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenBan.ForeColor = System.Drawing.Color.Black;
+            this.txtTenBan.Location = new System.Drawing.Point(934, 82);
+            this.txtTenBan.Name = "txtTenBan";
+            this.txtTenBan.Size = new System.Drawing.Size(117, 43);
+            this.txtTenBan.TabIndex = 28;
+            // 
+            // FDatBanAn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1232, 582);
+            this.Controls.Add(this.txtTenBan);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.txbTotalPrice);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lsvBill);
+            this.Controls.Add(this.flpFood);
+            this.Controls.Add(this.flpCategory);
+            this.Controls.Add(this.flpTable);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "FDatBanAn";
+            this.Text = "FDatBanAn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbTotalPrice;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lsvBill;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.FlowLayoutPanel flpFood;
+        private System.Windows.Forms.FlowLayoutPanel flpCategory;
+        private System.Windows.Forms.FlowLayoutPanel flpTable;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.TextBox txtTenBan;
+    }
+}
---
+++ Duan1/Solution2/GUI/FDatBanAn.cs
@@ -0,0 +1,278 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class FDatBanAn : Form
+    {
+        private ContextMenuStrip contextMenuStrip;
+        public FDatBanAn()
+        {
+            InitializeComponent();
+            LoadCategory();
+            LoadTable();
+            ResetUI();
+            InitializeContextMenuStrip();
+        }
+        private void ResetUI()
+        {
+            flpCategory.Visible = false;
+            flpFood.Visible = false;
+            lsvBill.Visible = false;
+            
+            button1.Visible = true;
+            flpTable.Visible = false;
+            button2.Visible = false;
+            txtTimKiem.Visible = false;
+            btnTimKiem.Visible = false;
+            txbTotalPrice.Visible = false;
+            txtTenBan.Visible = false;
+        }
+        private void LoadCategory()
+        {
+            List<Category> categoryList = CategoryDAL.Instance.GetListCategory();
+            foreach (Category item in categoryList)
+            {
+                //Button btn = new Button()
+                //{
+                //    Width = TableDAL.TableWidth,
+                //    Height = TableDAL.TableHeight,
+                //    Text = item.Name,
+                //    Tag = item
+                //};
+                CustomButton btn = new CustomButton
+                {
+                    BorderRadius = 20,
+                    BorderSize = 2,
+                    BorderColor = Color.PaleVioletRed,
+                    Size = new Size(150, 60),
+                    Text = item.Name,
+                    Tag = item,
+                    ForeColor = Color.White,
+                    BackColor = Color.Black
+                };
+                btn.Click += btnCategory_Click;
+
+                flpCategory.Controls.Add(btn);
+            }
+
+        }
+        private void LoadTable()
+        {
+            flpTable.Controls.Clear();
+            List<Table> tableList = TableDAL.Instance.LoadTableList();
+            foreach (Table item in tableList)
+            {
+                //Button btn = new Button()
+                //{
+                //    Width = 150,
+                //    Height = 60,
+                //    Text = item.TenBan + Environment.NewLine + item.TrangThai,
+                //    Tag = item
+                //};
+                CustomButton btn = new CustomButton()
+                {
+                    ForeColor = Color.Black,
+                    BorderRadius = 20,
+                    BorderSize = 2,
+                    BorderColor = Color.PaleVioletRed,
+                    Size = new Size(150, 80),
+                    Text = item.TenBan + Environment.NewLine + item.TrangThai,
+                    Tag = item
+                };
+                btn.Click += btnTable_Click;
+
+                btn.BackColor = item.TrangThai == "Có Người" ? Color.Red : Color.White;
+                flpTable.Controls.Add(btn);
+            }
+        }
+        private void InitializeContextMenuStrip()
+        {
+            contextMenuStrip = new ContextMenuStrip();
+
+
+
+            ToolStripMenuItem rightClickItem = new ToolStripMenuItem("Loại bỏ một phần thức ăn");
+            rightClickItem.Click += (sender, e) =>
+            {
+                btnFoodRight_Click(sender, e);
+            };
+
+            contextMenuStrip.Items.Add(rightClickItem);
+        }
+        private void ShowBill(int id)
+        {
+            lsvBill.Items.Clear();
+            List<DTO.Menu> listBillInfo = MenuDAL.Instance.GetListMenuBYTable(id);
+            float totalPrice = 0;
+            foreach (DTO.Menu item in listBillInfo)
+            {
+                ListViewItem lsvItem = new ListViewItem(item.FoodName.ToString());
+                lsvItem.SubItems.Add(item.Count.ToString());
+                lsvItem.SubItems.Add(item.Price.ToString());
+                lsvItem.SubItems.Add(item.TotalPrice.ToString());
+                totalPrice += item.TotalPrice;
+
+                lsvBill.Items.Add(lsvItem);
+            }
+            CultureInfo culture = new CultureInfo("vi-VN");
+            txbTotalPrice.Text = totalPrice.ToString("c", culture);
+        }
+        private void btn_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Button button = sender as Button;
+                contextMenuStrip.Tag = button; // Lưu button vào Tag của ContextMenuStrip để sử dụng sau này
+                contextMenuStrip.Show(button, e.Location);
+            }
+        }
+        private Image ByteArrayToImage(byte[] byteArray)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArray))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+        private void btnCategory_Click(object sender, EventArgs e)
+        {
+            int categoryID = ((sender as Button).Tag as Category).ID;
+            flpFood.Controls.Clear();
+            List<Food> listFood = FoodDAL.Instance.GetFoodByCategoryID(categoryID);
+            foreach (Food item in listFood)
+            {
+                CustomButton btn = new CustomButton
+                {
+                    Text = $"{item.TenMon}\n{item.Gia}",
+                    BorderRadius = 20,
+                    BorderSize = 2,
+                    Size = new Size(150, 150),
+                    Tag = item,
+                    TextAlign = ContentAlignment.BottomCenter,
+                    ImageAlign = ContentAlignment.TopCenter,
+                    BackColor = Color.White,
+                    ForeColor = Color.Black
+
+
+                };
+                Label lblText = new Label
+                {
+                    Size = new Size(180, 60),
+
+                };
+
+                // Convert the byte array to an Image and set it for the button if available
+                if (item.HinhMonAn != null && item.HinhMonAn.Length > 0)
+                {
+                    try
+                    {
+                        btn.Image = new Bitmap(ByteArrayToImage(item.HinhMonAn), new Size(btn.Width, btn.Height - 60)); // Adjust image size to fit the button
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error loading image: " + ex.Message);
+                    }
+                }
+
+                btn.Click += btnFood_Click;
+                btn.MouseUp += btn_MouseUp;
+                flpFood.Controls.Add(btn);
+            }
+        }
+
+        private void btnFood_Click(object sender, EventArgs e)
+        {
+            Button button = contextMenuStrip.Tag as Button;
+            Table table = lsvBill.Tag as Table;
+            int idBill = BillDAL.Instance.GetBillIDByTableID(table.MaBan);
+            int foodID = ((sender as Button).Tag as Food).IdMon;
+            int count = 1;
+
+            if (idBill == -1)
+            {
+                BillDAL.Instance.InsertBill(table.MaBan);
+                BillInfoDAL.Instance.InserBillInfo(BillDAL.Instance.GetMaxIDBill(), foodID, count);
+            }
+            else
+            {
+                BillInfoDAL.Instance.InserBillInfo(idBill, foodID, count);
+            }
+
+            ShowBill(table.MaBan);
+            LoadTable();
+        }
+
+        private void btnTable_Click(object sender, EventArgs e)
+        {
+            int tableID = ((sender as Button).Tag as Table).MaBan;
+            lsvBill.Tag = (sender as Button).Tag;
+            Table table = TableDAL.Instance.GetTablebyId(tableID).FirstOrDefault();
+            if (table != null)
+            {
+                txtTenBan.Text = table.TenBan; // Assuming 'TenBan' is the property for the table name
+            }
+
+            ShowBill(tableID);
+            flpCategory.Visible = true;
+            flpFood.Visible = true;
+            lsvBill.Visible = true;
+            
+            button2.Visible = true;
+            btnTimKiem.Visible = true;
+            txtTimKiem.Visible = true;
+            txbTotalPrice.Visible = true;
+            txtTenBan.Visible = true;
+        }
+
+        private void btnCheckOut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flpTable.Visible = true;
+            button1.Visible = false;
+            button2.Visible = true;
+            LoadTable();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResetUI();
+        }
+        private void btnFoodRight_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = sender as ToolStripMenuItem;
+            Button button = contextMenuStrip.Tag as Button;
+            Table table = lsvBill.Tag as Table;
+            int idBill = BillDAL.Instance.GetBillIDByTableID(table.MaBan);
+            int foodID = ((button.Tag as Food).IdMon);
+            int count = -1; // Số lượng âm để xóa sản phẩm khỏi hóa đơn
+
+            if (idBill == -1)
+            {
+                BillDAL.Instance.InsertBill(table.MaBan);
+                BillInfoDAL.Instance.InserBillInfo(BillDAL.Instance.GetMaxIDBill(), foodID, count);
+            }
+            else
+            {
+                BillInfoDAL.Instance.InserBillInfo(idBill, foodID, count);
+            }
+
+            ShowBill(table.MaBan);
+            LoadTable();
+        }
+    }
+}
